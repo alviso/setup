99be233f37612386c99c1834fe7fb8e7d2d43f83
Peter Varga <wmobil@gmail.com>
2022-04-17 20:31:02 -0700

    add wifi scan

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const scanner = require('node-wifi-scanner');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  scanner.scan((err, networks) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(networks);
+  });
+
   res.render('index', { title: 'Express' });
 });
 
